be21a9ecf314ad5f2c592dee13126a58a7ac9b9c
Johan Rosa <johan.rosaperez@gmail.com>
2023-04-20 16:57:12 -0400

    code to fetch and explore images

---
+++ scripts/test_reading_tif.R
@@ -0,0 +1,13 @@
+library(raster)
+library(tiff)
+
+
+files <- list.files("data/testing", pattern = ".tif", full.names = TRUE)
+
+target_file <- files[1]
+
+r1 <- raster(target_file)
+band4 <- raster(target_file, band = 4)
+s1 <- stack(target_file)
+
+plotRGB(s1, r = 3, g = 2, b = 1, stretch = "lin")
