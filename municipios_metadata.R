46236460f417a7eb363f1c2f0cb51360d081266a
Johan Rosa <johan.rosaperez@gmail.com>
2023-04-22 12:30:34 -0400

    Municipios metadata
    1. Script para preparar la metadata
    2. Exportar data en excel y RDS

---
+++ municipios_metadata.R
@@ -0,0 +1,97 @@
+# Paquetes ----------------------------------------------------------------
+library(sf)
+library(glue)
+library(stringr)
+library(dplyr)
+
+# Información territorial -------------------------------------------------
+provincias_metadata <- tibble::tribble(
+ ~region_code,                  ~region_label, ~provincia_code,         ~provincia_label,
+         "10", "Región Ozama O Metropolitana",            "01",      "Distrito Nacional",
+         "05",              "Región Valdesia",            "02",                   "Azua",
+         "06",            "Región Enriquillo",            "03",                "Baoruco",
+         "06",            "Región Enriquillo",            "04",               "Barahona",
+         "04",        "Región Cibao Noroeste",            "05",                "Dajabón",
+         "03",        "Región Cibao Nordeste",            "06",                 "Duarte",
+         "07",              "Región El Valle",            "07",             "Elías Piña",
+         "08",                  "Región Yuma",            "08",               "El Seibo",
+         "01",           "Región Cibao Norte",            "09",              "Espaillat",
+         "06",            "Región Enriquillo",            "10",          "Independencia",
+         "08",                  "Región Yuma",            "11",          "La Altagracia",
+         "08",                  "Región Yuma",            "12",              "La Romana",
+         "02",             "Región Cibao Sur",            "13",                "La Vega",
+         "03",        "Región Cibao Nordeste",            "14", "María Trinidad Sánchez",
+         "04",        "Región Cibao Noroeste",            "15",           "Monte Cristi",
+         "06",            "Región Enriquillo",            "16",             "Pedernales",
+         "05",              "Región Valdesia",            "17",                "Peravia",
+         "01",           "Región Cibao Norte",            "18",           "Puerto Plata",
+         "03",        "Región Cibao Nordeste",            "19",       "Hermanas Mirabal",
+         "03",        "Región Cibao Nordeste",            "20",                 "Samaná",
+         "05",              "Región Valdesia",            "21",          "San Cristóbal",
+         "07",              "Región El Valle",            "22",               "San Juan",
+         "09",               "Región Higuamo",            "23",   "San Pedro De Macorís",
+         "02",             "Región Cibao Sur",            "24",        "Sanchez Ramírez",
+         "01",           "Región Cibao Norte",            "25",               "Santiago",
+         "04",        "Región Cibao Noroeste",            "26",     "Santiago Rodríguez",
+         "04",        "Región Cibao Noroeste",            "27",               "Valverde",
+         "02",             "Región Cibao Sur",            "28",         "Monseñor Nouel",
+         "09",               "Región Higuamo",            "29",            "Monte Plata",
+         "09",               "Región Higuamo",            "30",             "Hato Mayor",
+         "05",              "Región Valdesia",            "31",       "San José De Ocoa",
+         "10", "Región Ozama O Metropolitana",            "32",          "Santo Domingo"
+ )
+
+# Importando data y shapefiles --------------------------------------------
+
+municipios <- rgdal::readOGR(dsn = "data/shapefiles/municipio/MUNCenso2010.shp") |> 
+  sp::spTransform(sp::CRS("+proj=longlat +datum=WGS84"))
+
+# En formato simple features
+municipios_sf <- st_as_sf(municipios) %>%
+  janitor::clean_names()
+
+# Agregar información sobre la división política --------------------------
+municipios_sf <- municipios_sf %>%
+  dplyr::select(
+    id = enlace,
+    region_code = reg,
+    provincia_code = prov,
+    municipio_code = mun,
+    municipio_label = toponimia,
+    geometry) %>%
+  mutate(municipio_label = str_to_title(municipio_label)) %>%
+  left_join(provincias_metadata, by = c("region_code", "provincia_code")) %>%
+  select(
+    id, region_code, region_label,
+    provincia_code, provincia_label,
+    municipio_code, municipio_label,
+    geometry)
+
+# Detalles geográficos ----------------------------------------------------
+centroids_and_bbox <- municipios_sf %>%
+  transmute(
+    municipio_centroid = purrr::map(
+      geometry, \(x) st_centroid(x) |> st_coordinates() |> as.data.frame()
+    ),
+    bbox = purrr::map(geometry, \(x){st_bbox(x) |> as.list() |> as.data.frame()})
+  ) %>%
+  st_drop_geometry()
+
+centroids <- centroids_and_bbox$municipio_centroid %>%
+  bind_rows() %>%
+  setNames(c("centroid_x", "centroid_y"))
+
+bbox <- centroids_and_bbox$bbox %>%
+  bind_rows()
+
+municipios_df <- municipios_sf %>%
+  bind_cols(centroids) %>%
+  bind_cols(bbox) %>%
+  st_drop_geometry() %>%
+  as_tibble()
+
+# Guardando archivos ------------------------------------------------------
+openxlsx::write.xlsx(municipios_df, "data/excel/municipios_metadata_new.xlsx")
+saveRDS(municipios_df, "data/rds/municipios_medatada.rds")
+saveRDS(municipios_sf, "data/rds/municipios_sf.rds")
+
